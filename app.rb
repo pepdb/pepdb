418e16629bf52c2616ce20e9422c17613d1d77e9
Christian Casar <0casar@informatik.uni-hamburg.de>
2013-08-22 01:30:01 +0200

    added more infos on element selections

--- app.rb
+++ app.rb
@@ -7,18 +7,18 @@ load 'model.rb'
 load 'helpers.rb'
 
 Haml::Options.defaults[:format] = :xhtml
-Haml::Options.defaults[:hypenate_data_attrs] = false
 
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 
-selection_columns = [:selection_name, :library_name, :date, :species, :tissue, :cell]
-dataset_columns = [:dataset_name, :library_name, :selection_name, :date, :selection_round]
+selection_columns = [:selection_name]
+selection_all_columns = [:selection_name, :library_name, :date, :species, :tissue, :cell]
+dataset_columns = [:dataset_name ]
+dataset_info_columns = [:dataset_name, :library_name, :selection_name, :date]
+dataset_all_columns = [:dataset_name, :library_name, :selection_name, :date, :selection_round, :sequence_length, :read_type, :used_indices, :origin, :sequencer, :produced_by, :species, :tissue, :cell, :statistics]
 peptide_columns = [:peptides__peptide_sequence ]
-
-#before %r{.+\.js} do
-#  content_type 'text/javascript'
-#end
+peptide_all_columns = [:peptides__peptide_sequence, :sequencing_datasets__dataset_name, :selection_name, :library_name, :rank, :reads, :dominance, :performance, :species, :tissue, :cell ]
+dna_columns = [:dna_sequences__dna_sequence, :reads]
 
 get '/' do
   haml :login
@@ -39,24 +39,53 @@ get '/libraries/:lib_name' do
   haml :libraries
 end
 
-get '/selections' do
+get '/libraries/:lib_name/:sel_name' do
+  @libraries = Library.all
   @selections = Selection.join(Target, :target_id=>:target_id).select(*selection_columns)
+  @selection_info = Selection.join(Target, :target_id=>:target_id).select(*selection_all_columns)
+  haml :libraries
+end
+
+get '/selections' do
+  @selections = Selection.join(Target, :target_id=>:target_id).select(*selection_all_columns)
   haml :selections
 end
 
 get '/selections/:sel_name' do
-  @selections = Selection.join(Target, :target_id=>:target_id).select(*selection_columns)
+  @selections = Selection.join(Target, :target_id=>:target_id).select(*selection_all_columns)
   @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_columns)
   haml :selections
 end
 
-get '/datasets' do
+get '/selections/:sel_name/:set_name' do
+  @selections = Selection.join(Target, :target_id=>:target_id).select(*selection_all_columns)
   @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_columns)
+  @dataset_info = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_all_columns)
+  haml :selections
+end
+
+get '/datasets' do
+  @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_info_columns)
   haml :datasets
 end
 
 get '/datasets/:set_name' do
-  @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_columns)
+  @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_info_columns)
   @peptides = Peptide.join(Observation, :peptide_sequence___peptide=>:peptide_sequence___peptide).join(SequencingDataset, :dataset_name___dataset=>:dataset_name).select(*peptide_columns)
   haml :datasets
 end
+  
+get '/datasets/:set_name/:pep_seq' do
+  @datasets = SequencingDataset.join(Target, :target_id=>:target_id).select(*dataset_info_columns)
+  @peptides = Peptide.join(Observation, :peptide_sequence___peptide=>:peptide_sequence___peptide).join(SequencingDataset, :dataset_name___dataset=>:dataset_name).select(*peptide_columns)
+  @peptide_info = Peptide.join(Observation, :peptide_sequence___peptide=>:peptide_sequence___peptide).join(SequencingDataset, :dataset_name=>:dataset_name).left_join(Result, :peptides_sequencing_datasets__result_id => :results__result_id).left_join(Target, :targets__target_id => :results__target_id).select(*peptide_all_columns)
+  @peptide_dna = Peptide.join(DNAFinding, :peptide_sequence=>:peptide_sequence).join(SequencingDataset, :dataset_name =>:dataset_name).join(DNASequence, :dna_sequence=>:dna_sequences_peptides_sequencing_datasets__dna_sequence).select(*dna_columns)
+  haml :datasets
+end
+
+get '/clusters' do
+  @clusters = Cluster
+  haml :clusters
+end
+
+
--- public/script/main.js
+++ public/script/main.js
@@ -3,10 +3,21 @@ $(document).ready(function(){
     $(this).toggleClass('highlight');
   });
 
+  $('#show_table tr').hover(function(){
+    $(this).toggleClass('highlight');
+  });
+
   $('#select_table tr:has(td)').click(function(){
     var selected_id = $(this).find("td:first").html();
     var route = window.location.pathname;
     $('body').load(route + "/" + selected_id); 
+  });
+
+  $('#show_table tr:has(td)').click(function(){
+    var first_choice =  $('#select_table').data('first-choice');
+    var selected_id = $(this).find("td:first").html();
+    var route = window.location.pathname;
+    $('body').load(route+ "/" + first_choice + "/" + selected_id); 
   });
   
 });
