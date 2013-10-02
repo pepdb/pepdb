abbc9df9df766f47f9b04310a8e2a0be2670c0e5
Christian Casar <0casar@informatik.uni-hamburg.de>
2013-10-02 19:03:32 +0200

    started adding comaprative cluster search

---
+++ modules/compclustersearch.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+
+module Sinatra
+  module ComparativeClusterSearch 
+    class CompClusterSearcher
+    end #class
+    
+
+    def find_unique_clusters
+    end #find_unique
+  end#module  
+  helpers ComparativeClusterSearch 
+end #module
